f2b6d80997701b05fd91fd5144bd25fbdc16a224
BenjaminLoubradou <loubradou.benjamin@hotmail.fr>
2019-05-30 23:55:54 +0200

    commit https ok and categories seeder ok

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,30 +12,30 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         //
-//        $category = new \App\Category();
-//        $category->nom = 'Films';
-//        $category->is_online = 1;
-//        $category->save();
-//
-//        $category2 = new \App\Category();
-//        $category2->nom = 'Séries TV';
-//        $category2->is_online = 1;
-//        $category2->save();
-//
-//        $category3 = new \App\Category();
-//        $category3->nom = 'Musique';
-//        $category3->is_online = 1;
-//        $category3->save();
-//
-//        $category4 = new \App\Category();
-//        $category4->nom = 'Jeux-Vidéos';
-//        $category4->is_online = 1;
-//        $category4->save();
-//
-//        $category5 = new \App\Category();
-//        $category5->nom = 'Sport';
-//        $category5->is_online = 1;
-//        $category5->save();
+        $category = new \App\Category();
+        $category->nom = 'Films';
+        $category->is_online = 1;
+        $category->save();
+
+        $category2 = new \App\Category();
+        $category2->nom = 'Séries TV';
+        $category2->is_online = 1;
+        $category2->save();
+
+        $category3 = new \App\Category();
+        $category3->nom = 'Musique';
+        $category3->is_online = 1;
+        $category3->save();
+
+        $category4 = new \App\Category();
+        $category4->nom = 'Jeux-Vidéos';
+        $category4->is_online = 1;
+        $category4->save();
+
+        $category5 = new \App\Category();
+        $category5->nom = 'Sport';
+        $category5->is_online = 1;
+        $category5->save();
 
         $sous_category = new \App\Category();
         $sous_category->nom = "Les Goonies";
